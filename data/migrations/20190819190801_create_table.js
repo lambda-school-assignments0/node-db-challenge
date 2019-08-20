53169bc639de7e7654e96b98e30d2595c84d92c5
schang1146 <sammychang2185@gmail.com>
2019-08-19 19:32:55 -0700

    edited migration titles

--- data/migrations/20190819190801_create_table.js
+++ data/migrations/20190819190801_create_table.js
@@ -3,23 +3,23 @@ exports.up = function(knex) {
     return knex.schema
         .createTable("projects", tbl => {
             tbl.increments();
-            tbl.string("project_name", 128)
+            tbl.string("name", 128)
                 .notNullable();
-            tbl.string("project_description", 128)
+            tbl.string("description", 128)
                 .notNullable();
         })
         .createTable("tasks", tbl => {
             tbl.increments();
-            tbl.string("task_description", 128)
+            tbl.string("description", 128)
                 .notNullable();
-            tbl.string("task_notes", 128)
+            tbl.string("notes", 128)
         })
         .createTable("resources", tbl => {
             tbl.increments();
-            tbl.string("resource_name", 128)
+            tbl.string("name", 128)
                 .notNullable()
                 .unique();
-            tbl.string("resource_description", 128)
+            tbl.string("description", 128)
         })
         .createTable("project_resources", tbl => {
             tbl.integer("project_id")
